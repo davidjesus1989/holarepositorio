8d2048d9f11ce15dab1d13e5e8db21b4dbf68ef9
Alumno <Alumno@E133L30111.esucomex.acad>
2022-06-08 19:39:03 -0400

    cambio de nombre

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          hello
+          David Maldonado develop
         </p>
         <a
           className="App-link"
